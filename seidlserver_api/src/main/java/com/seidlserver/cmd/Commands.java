4e8f79602b0d347ca6d9ae987ac2ebab6b67e22c
Jonas <seijod17@htl-kaindorf.at>
2021-04-14 19:06:57 +0200

    Didn't stage a file

--- seidlserver_api/src/main/java/com/seidlserver/cmd/Commands.java
+++ seidlserver_api/src/main/java/com/seidlserver/cmd/Commands.java
@@ -1,8 +1,20 @@
 package com.seidlserver.cmd;
+
 /*
     Created by: Jonas Seidl
     Date: 14.04.2021
     Time: 18:48
 */
 public class Commands {
+    public static final String[] memTotalCommand = new String[]{"cat", "/proc/meminfo", "|", "grep", "\"^MemTotal*\"", "|", "grep", "-Eo \"[0-9]*[0-9]\""};
+
+    public static void main(String[] args) {
+        try{
+            System.out.println(CommandExecutor.execute(memTotalCommand));
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
 }
+
+
