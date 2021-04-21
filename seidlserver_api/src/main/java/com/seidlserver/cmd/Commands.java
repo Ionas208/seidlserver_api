a956035628e4c3d56ab2119fda39a6c10005a857
Ionas <seijod17@htl-kaindorf.at>
2021-04-21 11:44:09 +0200

    Token now in Response Body

--- seidlserver_api/src/main/java/com/seidlserver/cmd/Commands.java
+++ seidlserver_api/src/main/java/com/seidlserver/cmd/Commands.java
@@ -1,5 +1,7 @@
 package com.seidlserver.cmd;
 
+import java.io.IOException;
+
 /*
     Created by: Jonas Seidl
     Date: 14.04.2021
@@ -11,4 +13,16 @@ public class Commands {
     public static final String[] shutdown = new String[]{"sudo", "shutdown", "-P", "now"};
     public static final String[] restart = new String[]{"sudo", "restart"};
     public static final String[] cpu = new String[]{"mpstat", "-o", "JSON"};
+    public static final String[] test = new String[]{"wsl", "cat", "/proc/meminfo"};
+
+    public static void main(String[] args) {
+        try {
+            String s = CommandExecutor.execute(test);
+            System.out.println(s);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
