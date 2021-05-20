b1d017ddf7de3d1048ac30ed7fb7f25b69a478e5
Ionas208 <seijod17@htl-kaindorf.at>
2021-05-20 16:01:50 +0000

    Fixing reboot command

--- seidlserver_api/src/main/java/com/seidlserver/cmd/Commands.java
+++ seidlserver_api/src/main/java/com/seidlserver/cmd/Commands.java
@@ -11,7 +11,7 @@ public class Commands {
     public static final String[] mem = new String[]{"cat", "/proc/meminfo"};
 
     public static final String[] shutdown = new String[]{"sudo", "shutdown", "-P", "now"};
-    public static final String[] restart = new String[]{"sudo", "restart"};
+    public static final String[] restart = new String[]{"sudo", "reboot"};
 
     public static final String[] cpu = new String[]{"mpstat", "-o", "JSON"};
 
